7bb38eb4f04cebf642ae06e2662cd88a96a66a2c
Tara L Andrews <tla@mit.edu>
2020-06-16 11:44:52 +0200

    update for newer English data

--- exercise/project/main.js
+++ exercise/project/main.js
@@ -1,6 +1,6 @@
 // The below code fetches the .json data using a HTTP GET request
 // Define our data source as JSON file
-var sourceURL = 'data.json';
+var sourceURL = 'https://raw.githubusercontent.com/DHUniWien/Teaching_CBS4DH/master/exercise/project/data.json';
 // Fetch API: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 fetch(sourceURL)
   .then(function (response) {
@@ -36,15 +36,15 @@ function outputData(person) {
 
   // REGEX RULES AND ADDING CSS CLASSES TO ROWS FOR TOGGLING
   // Add a class to actors
-  if ((new RegExp(/schauspieler/gi)).test(person.profession)) {
+  if ((new RegExp(/acto?r/gi)).test(person.profession)) {
     row.classList.add('actor');
   }
   // Add a class to Austrians
-  if ((new RegExp(/österreich/gi)).test(person.profession)) {
+  if ((new RegExp(/austria/gi)).test(person.profession)) {
     row.classList.add('austrian');
   }
   // Add a class to Auto*
-  if ((new RegExp(/auto/gi)).test(person.profession)) {
+  if ((new RegExp(/author|\bwriter/gi)).test(person.profession)) {
     row.classList.add('auto');
   }
 
